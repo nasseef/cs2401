5046429eaa77f17b3a788ad6ac076eb991b22e5b
Nasseef Abukamail <abukamai@ohio.edu>
2021-06-12 10:50:32 -0400

    minor edit

--- 02-dynamic-arrays.cc
+++ 02-dynamic-arrays.cc
@@ -16,6 +16,7 @@ int main(int argc, char const *argv[]) {
 
     //create a dynamic array of integers
     int *numbers = new int[10];
+
     //now you can access it just like static arrays using []
     numbers[0] = 5;
     //5 is now stored in the first location
@@ -25,15 +26,16 @@ int main(int argc, char const *argv[]) {
     //two ways to print the first element
     cout << "First element numbers[0]: " << numbers[0] << endl;
     cout << "First element *numbers: " << *numbers << endl;
+    
 
     //two ways to store elements in the array
     numbers[1] = 88;
     *(numbers + 2) = 77; //store 77 at index 2
     *(numbers + 3) = 66;
     //two ways to print element 2
-    cout << "First element numbers[2]: " << numbers[0] << endl;
+    cout << "First element numbers[2]: " << numbers[2] << endl;
     cout << "First element *(numbers+2): " << *(numbers+2) << endl;
-
+    
     //Display the first 4 elements in the array using pointers
     for (size_t i = 0; i < 4; i++)
     {
@@ -51,10 +53,11 @@ int main(int argc, char const *argv[]) {
 
     //display the first letter in the name
     cout << "First letter in name: " << *namePtr << endl;
-
+    
     //we can advance the pointer anywhere in the string name 
     //using the pointer namePtr
     //Let's say we want to display the last name starting at position 5
+
     namePtr += 5; //move the pointer to position 5
     cout << "Last name: " << namePtr << endl;
 
